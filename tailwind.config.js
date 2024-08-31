d928119db65e4fd8631e8129e2f64c79831efb58
ferdynand <ferdynandergypramudani11@gmail.com>
2024-08-31 15:48:17 +0700

    initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Fira: ["Fira Sans Condensed", "sans-serif"],
+      Itim: ["Itim", "cursive"]
+    }
+  },
+  plugins: [],
+}
