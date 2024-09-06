8ba68edefa9f329c0e2fb33ac51ad88bf1920246
ferdynand ergy pramudani <ferdynandergypramudani11@gmail.com>
2024-09-06 11:41:01 +0700

    best seller section done

--- src/main.js
+++ src/main.js
@@ -3,13 +3,48 @@ const navLink = document.querySelectorAll(".nav-link");
 const hamburger = document.getElementById("hamburger");
 
 hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("left-[0]");
-    hamburger.classList.toggle("ri-close-large-line");
+  navMenu.classList.toggle("left-[0]");
+  hamburger.classList.toggle("ri-close-large-line");
 });
 
-navLink.forEach(link => {
-    link.addEventListener("click", () => {
-        navMenu.classList.toggle("left-[0]");
-        hamburger.classList.toggle("ri-close-large-line");
-    });
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.toggle("left-[0]");
+    hamburger.classList.toggle("ri-close-large-line");
+  });
+});
+
+// swiperjs
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+  autoplay: {
+    delay: 2000,
+  },
+  spaceBetween: 30,
+  grabCursor: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1180: {
+      slidesPerView: 4,
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".button-next-c",
+    prevEl: ".button-prev-c",
+  },
 });
